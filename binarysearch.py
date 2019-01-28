40bea0235a4928a80a0fccd9685e6f4cf98a04ca
dk3003 <38868675+dk3003@users.noreply.github.com>
2019-01-28 20:13:13 +0530

    Decison tree classifier

---
+++ binarysearch.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  8 15:52:27 2018
+
+@author: Vineet pathak
+"""
+
+# the tuples consist of (delay time of train1, number of times)
+# tuples are (minutes, number of times)
+in_time = [(0, 22), (1, 19), (2, 17), (3, 18),
+           (4, 16), (5, 15), (6, 9), (7, 7),
+           (8, 4), (9, 3), (10, 3), (11, 2)]
+too_late = [(6, 6), (7, 9), (8, 12), (9, 17),
+            (10, 18), (11, 15), (12, 16), (13, 7),
+            (14, 8), (15, 5)]
+
+# matplotlib inline
+import matplotlib.pyplot as plt
+
+X, Y = zip(*in_time)
+X2, Y2 = zip(*too_late)
+bar_width = 0.9
+plt.bar(X, Y, bar_width, color="blue", alpha=0.75, label="in time")
+bar_width = 0.8
+plt.bar(X2, Y2, bar_width, color="red", alpha=0.75, label="too late")
+plt.legend(loc='upper right')
+plt.show()
+
+# We can write a 'classifier' function, which will give the probability for catching the connecting train:
+in_time_dict = dict(in_time)
+too_late_dict = dict(too_late)
+
+
+def catch_the_train(min):
+    s = in_time_dict.get(min, 0)
+    if s == 0:
+        return 0
+    else:
+        m = too_late_dict.get(min, 0)
+        return s / (s + m)
+
+
+for minutes in range(-1, 13):
+    print(minutes, catch_the_train(minutes))
+
+
